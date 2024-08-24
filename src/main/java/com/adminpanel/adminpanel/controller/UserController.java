75cb4aed98b5672ea37c6bc5365afbc86c12705f
Aryan Satani <141852653+Aryan0097@users.noreply.github.com>
2024-08-24 22:36:18 +0530

    Update UserController.java

--- src/main/java/com/adminpanel/adminpanel/controller/UserController.java
+++ src/main/java/com/adminpanel/adminpanel/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@CrossOrigin("http://127.0.0.1:5500")
+@CrossOrigin("localhost")
 @RestController
 public class UserController {
     
